3c45e0cdf754cb099e9833b70d06553d8c582409
Geo-sudo <george.nady.contact@gmail.com>
2024-08-30 21:55:23 +0300

    Fix for Cloudflare.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,40 +1,32 @@
-import { defineConfig } from 'astro/config'
-import mdx from '@astrojs/mdx'
-import sitemap from '@astrojs/sitemap'
-import tailwind from '@astrojs/tailwind'
-import { remarkReadingTime } from './src/utils/readTime.ts'
-import image from '@astrojs/image';
-
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import tailwind from '@astrojs/tailwind';
+import { remarkReadingTime } from './src/utils/readTime.ts';
 
 // https://astro.build/config
 export default defineConfig({
-	
-	site: 'https://geosalib.com', // Write here your website url
-	output: 'static',
-	markdown: {
-		remarkPlugins: [remarkReadingTime],
-		drafts: true,
-		shikiConfig: {
-			theme: 'material-theme-palenight',
-			wrap: true
-		}
-	},
-	integrations: [
-		mdx({
-			syntaxHighlight: 'shiki',
-			shikiConfig: {
-				experimentalThemes: {
-					light: 'vitesse-light',
-					dark: 'material-theme-palenight',
-				  },
-				wrap: true
-			},
-			drafts: true
-		}),
-		sitemap(),
-		tailwind(),
-		image({
-			serviceEntryPoint: '@astrojs/image/sharp',
-		  }),
-	]
-})
+  site: 'https://geosalib.com',
+  // Write here your website url
+  output: 'static',
+  markdown: {
+    remarkPlugins: [remarkReadingTime],
+    drafts: true,
+    shikiConfig: {
+      theme: 'material-theme-palenight',
+      wrap: true
+    }
+  },
+  integrations: [mdx({
+    syntaxHighlight: 'shiki',
+    shikiConfig: {
+      experimentalThemes: {
+        light: 'vitesse-light',
+        dark: 'material-theme-palenight'
+      },
+      wrap: true
+    },
+    drafts: true
+  }), sitemap(), tailwind()],
+
+});
